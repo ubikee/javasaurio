3d3cb590525928d0054d02d45555ba6248f2837a
jeroldangarcia <jeroldan@gmail.com>
2014-02-28 22:08:38 +0000

    base

---
+++ app.js
@@ -0,0 +1,30 @@
+/**
+* Created with javasaurio.
+* User: jeroldangarcia
+* Date: 2014-02-26
+* Time: 05:38 PM
+* To change this template use Tools | Templates.
+*/
+var flash = require('connect-flash')
+, express = require('express')
+, http = require('http')
+, path = require('path')
+, util = require('util')
+, cons = require('consolidate');
+
+var app = express();
+app.engine('html', cons.swig);
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname+'/html');
+app.set('view engine', 'html');
+app.use(express.logger());
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use('/bower_components', express.static(__dirname+'/bower_components'));
+app.use('/widgets', express.static(__dirname+'/widgets'));
+app.use(express.static(__dirname + '/public'));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
